81bdfc18c94f77f77ddd02234ee46a7e0375028b
GitAniket98 <aniketchaudhary9838@gmail.com>
2026-04-03 18:20:41 +0000

    utils: added ApiError, ApiResponse, and AsyncHandler

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,12 @@
+class ApiError extends Error {
+  statusCode: number
+  errors: string[]
+
+  constructor(statusCode: number, message: string, errors: string[] = []) {
+    super(message)
+    this.statusCode = statusCode
+    this.errors = errors
+  }
+}
+
+export default ApiError
